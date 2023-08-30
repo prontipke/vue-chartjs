852e1cf5bfee1109404bda7af44f76a1e36f1fdc
Prontip <prontip.ke@gmail.com>
2023-08-30 13:33:26 +0700

    เพิ่ม custom Datalabels และ custom Tooltip

--- src/App.vue
+++ src/App.vue
@@ -17,9 +17,14 @@
                 :maintainAspectRatio="false"
                 :setHeightChart="setHeightBarChart"
                 :disableLegend="disableLegendBarChart"
-                positionDatalabels="top"
+                positionDatalabels="center"
                 minWidth="600"
                 axis="y"
+                datalabelsPercent
+                customTooltip
+                :dataCustomTooltip="aa"
+                customDatalabels
+                :dataCustomDatalabels="aa"
             />
         </div>
     </div>
@@ -38,6 +43,36 @@ export default {
     data() {
         return {
             // line
+            aa: {
+                // data:[
+                //     { label:'รพ.1', sum_adjrw: '1', case_count: '11', compensation: '111' },
+                //     { label:'รพ.2', sum_adjrw: '2', case_count: '22', compensation: '222' },
+                //     { label:'รพ.3',sum_adjrw: '3', case_count: '33', compensation: '333' },
+                // ],
+                data: [
+                    {
+                        label: 'Votes1',
+                        data: [
+                            { label: 'รพ.1', sum_adjrw: '1Votes1รพ.1', case_count: '11', compensation: '111' },
+                            { label: 'รพ.2', sum_adjrw: '2Votes1รพ.2', case_count: '22', compensation: '222' },
+                            { label: 'รพ.3', sum_adjrw: '3Votes1รพ.3', case_count: '33', compensation: '333' },
+                        ],
+                    },
+                    {
+                        label: 'Votes2',
+                        data: [
+                            { label: 'รพ.1', sum_adjrw: '1Votes2รพ.1', case_count: '11', compensation: '111' },
+                            { label: 'รพ.2', sum_adjrw: '2Votes2รพ.2', case_count: '22', compensation: '222' },
+                            { label: 'รพ.3', sum_adjrw: '3Votes2รพ.3', case_count: '33', compensation: '333' },
+                        ],
+                    },
+                ],
+                index: [
+                    { index: 'sum_adjrw', text: 'Sum AdjRW', unit: '' },
+                    { index: 'case_count', text: 'จำนวนเคส', unit: 'เคส' },
+                    { index: 'compensation', text: 'ค่าชดเชย', unit: 'บาท' },
+                ],
+            },
             setHeightChart: '200',
             showDatalabels: true,
             disableLegend: true,
@@ -77,12 +112,18 @@ export default {
             setHeightBarChart: '00',
             showDatalabelsBarChart: true,
             disableLegendBarChart: true,
-            labelsBarChart: ['รพ.1', 'รพ.2', 'รพ.3', 'รพ.4', 'รพ.5', 'รพ.6'],
+            labelsBarChart: ['รพ.1', 'รพ.2', 'รพ.3'],
             datasetsBarChart: [
                 {
-                    label: 'Votes',
-                    data: [700, 800, 900, 100, 110, 120],
-                    backgroundColor: ['#003eff', '#003eff', '#003eff', '#003eff', '#003eff', '#003eff'],
+                    label: 'Votes1',
+                    data: [400, 600, 500],
+                    backgroundColor: ['#003eff', '#003eff', '#003eff'],
+                    labelColortext: '#1e1e1e', //default black
+                },
+                {
+                    label: 'Votes2',
+                    data: [600, 400, 500],
+                    backgroundColor: ['#ff2d00', '#ff2d00', '#ff2d00'],
                     labelColortext: '#1e1e1e', //default black
                 },
             ],
