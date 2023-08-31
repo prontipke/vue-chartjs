68859e8b7f429b5feb7126c84c0885b2a217cd0f
Prontip <prontip.ke@gmail.com>
2023-08-31 16:23:18 +0700

    register pie chart

--- src/install.js
+++ src/install.js
@@ -1,9 +1,11 @@
 import lineChart from './components/lineChart.vue'
 import barChart from './components/barChart.vue'
+import pieChart from './components/pieChart.vue'
 const ComponentChart = {
   install(Vue) {
     Vue.component('mai-line-chartjs', lineChart)
     Vue.component('mai-bar-chartjs', barChart)
+    Vue.component('mai-pie-chartjs', pieChart)
   },
 }
 if (typeof window !== 'undefined' && window.Vue) {
